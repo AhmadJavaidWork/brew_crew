2281a9ba92a2be1f318005716b7d5181a3102792
rv <ahmadjavaidwork@gmail.com>
2020-06-06 18:28:05 +0500

    defined the basic structure of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:brew_crew/screens/wrapper.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -6,11 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Brew Crew'),
-        ),
-      ),
+      home: Wrapper(),
     );
   }
 }
