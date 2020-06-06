6527198d2577eed9f2343d804913ba71c463e3d5
rv <ahmadjavaidwork@gmail.com>
2020-06-06 19:07:50 +0500

    anonymous sign in

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,38 @@
+import 'package:brew_crew/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final AuthService _auth = AuthService();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red[900],
+        elevation: 0.0,
+        title: Text('Sign in to Brew Crew'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+        child: RaisedButton(
+          child: Text(
+            'Sign in anon',
+          ),
+          onPressed: () async {
+            dynamic result = await _auth.signInAnon();
+            if (result == null) {
+              print('Error signing in');
+            } else {
+              print('Signed in');
+              print(result);
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
