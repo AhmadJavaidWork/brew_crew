7f4a556b5d312a1305db21fe261dbdedf8c0605e
rv <ahmadjavaidwork@gmail.com>
2020-06-07 15:23:36 +0500

    updating user data

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -11,14 +11,7 @@ class BrewList extends StatefulWidget {
 class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
-    final brews = Provider.of<List<Brew>>(context);
-    brews.forEach(
-      (brew) {
-        print(brew.name);
-        print(brew.sugars);
-        print(brew.strength);
-      },
-    );
+    final brews = Provider.of<List<Brew>>(context) ?? [];
     return ListView.builder(
       itemCount: brews.length,
       itemBuilder: (context, index) {
--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -20,74 +20,79 @@ class _SettingsFormState extends State<SettingsForm> {
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
     return StreamBuilder<UserData>(
-        stream: DataBaseService(uid: user.uid).userData,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            UserData userData = snapshot.data;
-            return Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    'Update your brew settings.',
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    initialValue: userData.name,
-                    decoration: textInputDecoration,
-                    validator: (value) =>
-                        value.isEmpty ? 'Please enter a name' : null,
-                    onChanged: (value) => setState(() => _currentName = value),
-                  ),
-                  SizedBox(height: 20.0),
-                  DropdownButtonFormField(
-                    decoration: textInputDecoration,
-                    value: _currentSugars ?? userData.sugars,
-                    items: sugars.map(
-                      (sugar) {
-                        return DropdownMenuItem(
-                          value: sugar,
-                          child: Text('$sugar sugars'),
-                        );
-                      },
-                    ).toList(),
-                    onChanged: (value) =>
-                        setState(() => _currentSugars = value),
-                  ),
-                  SizedBox(height: 20.0),
-                  Slider(
-                    value: (_currentStrength ?? userData.strength).toDouble(),
-                    min: 100.0,
-                    max: 900.0,
-                    divisions: 8,
-                    onChanged: (value) => setState(
-                      () => _currentStrength = value.round(),
-                    ),
-                    inactiveColor:
-                        Colors.brown[_currentStrength ?? userData.strength],
-                    activeColor:
-                        Colors.brown[_currentStrength ?? userData.strength],
-                  ),
-                  SizedBox(height: 20.0),
-                  RaisedButton(
-                    color: Colors.red[700],
-                    child: Text(
-                      'Update',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onPressed: () async {
-                      print(_currentName);
-                      print(_currentSugars);
-                      print(_currentStrength);
+      stream: DataBaseService(uid: user.uid).userData,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          UserData userData = snapshot.data;
+          return Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Update your brew settings.',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  initialValue: userData.name,
+                  decoration: textInputDecoration,
+                  validator: (value) =>
+                      value.isEmpty ? 'Please enter a name' : null,
+                  onChanged: (value) => setState(() => _currentName = value),
+                ),
+                SizedBox(height: 20.0),
+                DropdownButtonFormField(
+                  decoration: textInputDecoration,
+                  value: _currentSugars ?? userData.sugars,
+                  items: sugars.map(
+                    (sugar) {
+                      return DropdownMenuItem(
+                        value: sugar,
+                        child: Text('$sugar sugars'),
+                      );
                     },
+                  ).toList(),
+                  onChanged: (value) => setState(() => _currentSugars = value),
+                ),
+                SizedBox(height: 20.0),
+                Slider(
+                  value: (_currentStrength ?? userData.strength).toDouble(),
+                  min: 100.0,
+                  max: 900.0,
+                  divisions: 8,
+                  onChanged: (value) => setState(
+                    () => _currentStrength = value.round(),
                   ),
-                ],
-              ),
-            );
-          } else {
-            return Loading();
-          }
-        });
+                  inactiveColor:
+                      Colors.brown[_currentStrength ?? userData.strength],
+                  activeColor:
+                      Colors.brown[_currentStrength ?? userData.strength],
+                ),
+                SizedBox(height: 20.0),
+                RaisedButton(
+                  color: Colors.red[700],
+                  child: Text(
+                    'Update',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      await DataBaseService(uid: user.uid).updateUserData(
+                        sugars: _currentSugars ?? userData.sugars,
+                        name: _currentName ?? userData.name,
+                        strength: _currentStrength ?? userData.strength,
+                      );
+                      Navigator.pop(context);
+                    } else {}
+                  },
+                ),
+              ],
+            ),
+          );
+        } else {
+          return Loading();
+        }
+      },
+    );
   }
 }
