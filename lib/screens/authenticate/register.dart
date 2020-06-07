62872939a56bf2a56fdc54d3e62ba6f862f6d566
rv <ahmadjavaidwork@gmail.com>
2020-06-07 12:22:24 +0500

    implemented loading widget

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,5 +1,6 @@
 import 'package:brew_crew/services/auth.dart';
 import 'package:brew_crew/shared/constants.dart';
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -12,6 +13,7 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   // text field state
   String email = '';
@@ -19,101 +21,111 @@ class _RegisterState extends State<Register> {
   String error = '';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red[900],
-        elevation: 0.0,
-        title: Text('Sign up to Brew Crew'),
-        actions: <Widget>[
-          FlatButton.icon(
-            onPressed: widget.toggleView,
-            icon: Icon(
-              Icons.person,
-              color: Colors.white,
-            ),
-            label: Text(
-              'Sign In',
-              style: TextStyle(color: Colors.white),
-            ),
-          )
-        ],
-      ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 20.0,
-              ),
-              TextFormField(
-                validator: (value) => value.isEmpty ? 'Enter an email' : null,
-                onChanged: (value) {
-                  setState(() {
-                    email = value;
-                  });
-                },
-                decoration: textInputDecoration.copyWith(
-                  hintText: 'Email',
-                ),
-              ),
-              SizedBox(
-                height: 25.0,
-              ),
-              TextFormField(
-                validator: (value) =>
-                    value.length < 6 ? 'Enter a password 6+ chars long' : null,
-                obscureText: true,
-                onChanged: (value) {
-                  setState(() {
-                    password = value;
-                  });
-                },
-                decoration: textInputDecoration.copyWith(
-                  hintText: 'Password',
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              RaisedButton(
-                color: Colors.red[700],
-                elevation: 0.0,
-                child: Text(
-                  'Sign up',
-                  style: TextStyle(
+    return loading
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.red[900],
+              elevation: 0.0,
+              title: Text('Sign up to Brew Crew'),
+              actions: <Widget>[
+                FlatButton.icon(
+                  onPressed: widget.toggleView,
+                  icon: Icon(
+                    Icons.person,
                     color: Colors.white,
                   ),
+                  label: Text(
+                    'Sign In',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+            body: Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      validator: (value) =>
+                          value.isEmpty ? 'Enter an email' : null,
+                      onChanged: (value) {
+                        setState(() {
+                          email = value;
+                        });
+                      },
+                      decoration: textInputDecoration.copyWith(
+                        hintText: 'Email',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25.0,
+                    ),
+                    TextFormField(
+                      validator: (value) => value.length < 6
+                          ? 'Enter a password 6+ chars long'
+                          : null,
+                      obscureText: true,
+                      onChanged: (value) {
+                        setState(() {
+                          password = value;
+                        });
+                      },
+                      decoration: textInputDecoration.copyWith(
+                        hintText: 'Password',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    RaisedButton(
+                      color: Colors.red[700],
+                      elevation: 0.0,
+                      child: Text(
+                        'Sign up',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          setState(() {
+                            loading = true;
+                          });
+                          dynamic result =
+                              await _auth.registerWithEmailAndPassword(
+                            email,
+                            password,
+                          );
+                          if (result == null) {
+                            setState(() {
+                              error = 'please supply a valid email';
+                              loading = false;
+                            });
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(
+                      height: 12.0,
+                    ),
+                    Text(
+                      error,
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ],
                 ),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    dynamic result = await _auth.registerWithEmailAndPassword(
-                      email,
-                      password,
-                    );
-                    if (result == null) {
-                      setState(() {
-                        error = 'please supply a valid email';
-                      });
-                    }
-                  }
-                },
               ),
-              SizedBox(
-                height: 12.0,
-              ),
-              Text(
-                error,
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 14.0,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,6 @@
 import 'package:brew_crew/services/auth.dart';
 import 'package:brew_crew/shared/constants.dart';
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -12,6 +13,7 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   // text field state
   String email = '';
@@ -19,101 +21,112 @@ class _SignInState extends State<SignIn> {
   String error = '';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red[900],
-        elevation: 0.0,
-        title: Text('Sign in to Brew Crew'),
-        actions: <Widget>[
-          FlatButton.icon(
-            onPressed: widget.toggleView,
-            icon: Icon(
-              Icons.person_add,
-              color: Colors.white,
-            ),
-            label: Text(
-              'Register',
-              style: TextStyle(color: Colors.white),
-            ),
-          )
-        ],
-      ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 20.0,
-              ),
-              TextFormField(
-                validator: (value) => value.isEmpty ? 'Enter an email' : null,
-                onChanged: (value) {
-                  setState(() {
-                    email = value;
-                  });
-                },
-                decoration: textInputDecoration.copyWith(
-                  hintText: 'Email',
-                ),
-              ),
-              SizedBox(
-                height: 25.0,
-              ),
-              TextFormField(
-                validator: (value) =>
-                    value.length < 6 ? 'Enter a password 6+ chars long' : null,
-                obscureText: true,
-                onChanged: (value) {
-                  setState(() {
-                    password = value;
-                  });
-                },
-                decoration: textInputDecoration.copyWith(
-                  hintText: 'Password',
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              RaisedButton(
-                color: Colors.red[700],
-                elevation: 0.0,
-                child: Text(
-                  'Sign in',
-                  style: TextStyle(
+    return loading
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.red[900],
+              elevation: 0.0,
+              title: Text('Sign in to Brew Crew'),
+              actions: <Widget>[
+                FlatButton.icon(
+                  onPressed: widget.toggleView,
+                  icon: Icon(
+                    Icons.person_add,
                     color: Colors.white,
                   ),
+                  label: Text(
+                    'Register',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+            body: Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      validator: (value) =>
+                          value.isEmpty ? 'Enter an email' : null,
+                      onChanged: (value) {
+                        setState(() {
+                          email = value;
+                        });
+                      },
+                      decoration: textInputDecoration.copyWith(
+                        hintText: 'Email',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25.0,
+                    ),
+                    TextFormField(
+                      validator: (value) => value.length < 6
+                          ? 'Enter a password 6+ chars long'
+                          : null,
+                      obscureText: true,
+                      onChanged: (value) {
+                        setState(() {
+                          password = value;
+                        });
+                      },
+                      decoration: textInputDecoration.copyWith(
+                        hintText: 'Password',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    RaisedButton(
+                      color: Colors.red[700],
+                      elevation: 0.0,
+                      child: Text(
+                        'Sign in',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          setState(() {
+                            loading = true;
+                          });
+                          dynamic result =
+                              await _auth.signInWithEmailAndPassword(
+                            email,
+                            password,
+                          );
+                          if (result == null) {
+                            setState(() {
+                              error =
+                                  'Could not sign in with those credentials';
+                              loading = false;
+                            });
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(
+                      height: 12.0,
+                    ),
+                    Text(
+                      error,
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ],
                 ),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    dynamic result = await _auth.signInWithEmailAndPassword(
-                      email,
-                      password,
-                    );
-                    if (result == null) {
-                      setState(() {
-                        error = 'Could not sign in with those credentials';
-                      });
-                    }
-                  }
-                },
               ),
-              SizedBox(
-                height: 12.0,
-              ),
-              Text(
-                error,
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 14.0,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
