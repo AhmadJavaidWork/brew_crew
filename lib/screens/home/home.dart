1f0b0afedde2235e632c75ecdfa39a432e23c4d1
rv <ahmadjavaidwork@gmail.com>
2020-06-07 13:21:59 +0500

    setup brew model

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,15 +1,15 @@
+import 'package:brew_crew/models/brew.dart';
 import 'package:brew_crew/screens/home/brew_list.dart';
 import 'package:brew_crew/services/auth.dart';
 import 'package:brew_crew/services/database.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<QuerySnapshot>.value(
+    return StreamProvider<List<Brew>>.value(
       value: DataBaseService().brews,
       child: Scaffold(
         appBar: AppBar(
